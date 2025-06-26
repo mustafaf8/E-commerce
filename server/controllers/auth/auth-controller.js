2fe8d448e49ffbd8a02f9d34327610d6019dfa50
mustafaf8 <mustafanahsan754@gmail.com>
2025-06-26 16:19:22 +0300

    some google AuthO2 bug fix

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -21,7 +21,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: isProduction
+        ? `${process.env.CLIENT_BASE_URL}/api/auth/google/callback`
+        : "/api/auth/google/callback",
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
