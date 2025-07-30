29f2096826950885f77913bdb81ed1e7aadf84a1
mustafaf8 <mustafanahsan754@gmail.com>
2025-07-30 11:06:17 +0300

    Ürün yönetimi sayfasında kategori bilgisi güncellendi, ürün silme işlemi için onay modalı iyileştirildi. Ayrıca, arama önerileri için kategori menüsü görünürlüğü güncellendi ve gereksiz resim dosyası kaldırıldı.

--- client/src/pages/shopping-view/checkout.jsx
+++ client/src/pages/shopping-view/checkout.jsx
@@ -1,5 +1,5 @@
 import Address from "@/components/shopping-view/address";
-import img from "/tutu.jpg";
+import img from "/tutu.png";
 import { useDispatch, useSelector } from "react-redux";
 import UserCartItemsContent from "@/components/shopping-view/cart-items-content";
 import { Button } from "@/components/ui/button";
